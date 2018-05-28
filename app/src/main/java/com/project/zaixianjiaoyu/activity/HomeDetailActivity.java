7edce58512d106fb81590b3d16ebd4856772ee88
912348141@qq.com <314159zhang>
2018-05-28 14:51:00 +0800

    搭建登录界面

--- app/src/main/java/com/project/zaixianjiaoyu/activity/HomeDetailActivity.java
+++ app/src/main/java/com/project/zaixianjiaoyu/activity/HomeDetailActivity.java
@@ -1,15 +1,16 @@
 package com.project.zaixianjiaoyu.activity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.project.zaixianjiaoyu.R;
 import com.project.zaixianjiaoyu.statusbar.ImmersionBar;
+import com.project.zaixianjiaoyu.util.ToastUtil;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,6 +32,8 @@ public class HomeDetailActivity extends BaseActivity {
     TextView textTime;
     @BindView(R.id.text_content)
     TextView textContent;
+    @BindView(R.id.btn_collect)
+    Button btnCollect;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,7 +47,7 @@ public class HomeDetailActivity extends BaseActivity {
                 .init();
     }
 
-    @OnClick({R.id.img_back, R.id.rl_back, R.id.tv_title, R.id.rl_top_layout, R.id.text_title, R.id.text_time, R.id.text_content})
+    @OnClick({R.id.img_back, R.id.rl_back, R.id.tv_title, R.id.rl_top_layout, R.id.text_title, R.id.text_time, R.id.text_content,R.id.btn_collect})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.img_back:
@@ -62,6 +65,10 @@ public class HomeDetailActivity extends BaseActivity {
                 break;
             case R.id.text_content:
                 break;
+            case R.id.btn_collect:
+                ToastUtil.showCenterToast(this,"收藏成功");
+                break;
         }
     }
+
 }
