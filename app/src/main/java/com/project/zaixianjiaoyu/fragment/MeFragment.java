0fbc59c6bdcb9082390821a1c1e7ef1faa68e7de
912348141@qq.com <314159zhang>
2018-05-29 11:32:34 +0800

    搭建个人中心界面

--- app/src/main/java/com/project/zaixianjiaoyu/fragment/MeFragment.java
+++ app/src/main/java/com/project/zaixianjiaoyu/fragment/MeFragment.java
@@ -1,16 +1,15 @@
 package com.project.zaixianjiaoyu.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.project.zaixianjiaoyu.R;
-import com.project.zaixianjiaoyu.activity.Loginactivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,9 +20,27 @@ import butterknife.Unbinder;
 /**
  * Created by Administrator on 2015/12/11.
  */
-public class MeFragment extends Fragment {
+public class MeFragment extends BaseFragment {
 
     Unbinder unbinder;
+    @BindView(R.id.img_person)
+    ImageView imgPerson;
+    @BindView(R.id.text_name)
+    TextView textName;
+    @BindView(R.id.text_qiandao)
+    TextView textQiandao;
+    @BindView(R.id.text_touxiang)
+    TextView textTouxiang;
+    @BindView(R.id.text_kaoshi)
+    TextView textKaoshi;
+    @BindView(R.id.text_goumai)
+    TextView textGoumai;
+    @BindView(R.id.text_history)
+    TextView textHistory;
+    @BindView(R.id.text_fankui)
+    TextView textFankui;
+    @BindView(R.id.text_settings)
+    TextView textSettings;
 
     @Nullable
     @Override
@@ -42,4 +59,36 @@ public class MeFragment extends Fragment {
     }
 
 
+    @OnClick({R.id.img_person, R.id.text_name, R.id.text_qiandao, R.id.text_touxiang, R.id.text_kaoshi, R.id.text_goumai, R.id.text_history, R.id.text_fankui, R.id.text_settings})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.img_person:
+
+
+                break;
+            case R.id.text_name:
+                break;
+            case R.id.text_qiandao:
+                Toast.makeText(getActivity(), "签到", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.text_touxiang:
+                Toast.makeText(getActivity(), "头像设置", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.text_kaoshi:
+                Toast.makeText(getActivity(), "考试", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.text_goumai:
+                Toast.makeText(getActivity(), "购买课程", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.text_history:
+                Toast.makeText(getActivity(), "历史", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.text_fankui:
+                Toast.makeText(getActivity(), "意见反馈", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.text_settings:
+                Toast.makeText(getActivity(), "设置", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
